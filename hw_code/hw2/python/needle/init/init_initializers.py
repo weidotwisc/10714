0edf8e4ffa7c8964b2ce2270052fd1f3f0788c0a
Wei Zhang <weiz@Weis-MacBook-Pro.local>
2024-01-13 10:45:42 -0500

    finished hw2 weights initialization

--- hw_code/hw2/python/needle/init/init_initializers.py
+++ hw_code/hw2/python/needle/init/init_initializers.py
@@ -4,25 +4,35 @@ from .init_basic import *
 
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    a = gain * math.sqrt(6/(fan_in+fan_out))
+    data = rand(fan_in, fan_out, low=-a, high=a)
+    return data
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2 / (fan_in+fan_out))
+    data = randn(fan_in, fan_out, mean=0.0, std=std)
+    return data
     ### END YOUR SOLUTION
 
 
 def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    bound = gain * math.sqrt(3 / fan_in)
+    data = rand(fan_in, fan_out, low=-bound, high=bound)
+    return data
     ### END YOUR SOLUTION
 
 
 def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    std = gain / math.sqrt(fan_in)
+    data = randn(fan_in, fan_out, mean=0.0, std=std)
+    return data
     ### END YOUR SOLUTION
