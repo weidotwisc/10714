2da6c1f84c5d4fc4492bcc932635b22f2c6db0f8
Wei Zhang <weiz@weis-mbp.lan>
2024-01-23 23:25:16 -0500

    discovered the keepdims=True trick for summation and max

--- hw_code/hw2/python/needle/ops/ops_logarithmic.py
+++ hw_code/hw2/python/needle/ops/ops_logarithmic.py
@@ -25,11 +25,22 @@ def logsoftmax(a):
 
 class LogSumExp(TensorOp):
     def __init__(self, axes: Optional[tuple] = None):
-        self.axes = axes
+        if (axes is not None):
+            if (type(axes) is not tuple):
+                assert (type(axes) is int)
+                self.axes = (axes,)  # make it like sum axes semantic in weiz needle (aka sum semantic in numpy) weiz 2023-01-23
+            else:
+                self.axes = axes
+        else:
+            self.axes = None
 
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        max_z = array_api.max(Z, axis=self.axes, keepdims=True) # keep the annihilated axes as dimension 1, that is long time what I want
+        z_minus_z_max = Z - max_z
+        sum_exp_z = array_api.sum(array_api.exp(z_minus_z_max), axis=self.axes, keepdims=True) # similar to max, keep the annihiated axes as dimension 1
+        assert(sum_exp_z.shape == max_z.shape)
+        return array_api.sum(array_api.log(sum_exp_z) + max_z, axis=self.axes) # in order to make the semantic correspond to keepdims=False, the result need a reduction of axes, but the actual number stay the same
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
