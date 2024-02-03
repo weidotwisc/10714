c4daae15f614dd5d4c89694b93cd9a5c1d5d3e59
Wei Zhang <weiz@Weis-MacBook-Pro.local>
2024-02-02 19:37:27 -0500

    residual module supported.

--- hw_code/hw2/python/needle/nn/nn_basic.py
+++ hw_code/hw2/python/needle/nn/nn_basic.py
@@ -248,5 +248,5 @@ class Residual(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return x + self.fn(x)
         ### END YOUR SOLUTION
