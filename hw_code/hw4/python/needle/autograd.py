3d051c8902b5b53b7b27a48fbbb8881ea7c740b7
Wei Zhang <weiz@us.ibm.com>
2024-06-30 08:15:55 -0400

    better support for sequeeze() method to support Split op.

--- hw_code/hw4/python/needle/autograd.py
+++ hw_code/hw4/python/needle/autograd.py
@@ -188,7 +188,7 @@ class TensorTuple(Value):
 
     def detach(self):
         """Create a new tensor that shares the data but detaches from the graph."""
-        return Tuple.make_const(self.realize_cached_data())
+        return TensorTuple.make_const(self.realize_cached_data())
 
 
 class Tensor(Value):
@@ -216,7 +216,7 @@ class Tensor(Value):
                     array.numpy(), device=device, dtype=dtype
                 )
         else:
-            device = device if device else cpu()
+            device = device if device else default_device()
             cached_data = Tensor._array_from_numpy(array, device=device, dtype=dtype)
 
         self._init(
