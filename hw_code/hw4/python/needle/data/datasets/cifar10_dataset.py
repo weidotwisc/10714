5e128306c4ec9f63fd6c5188fb11cc029d0e81f7
Wei Zhang <weiz@us.ibm.com>
2024-07-07 13:56:08 -0400

    implemented CIFAR10Dataset

--- hw_code/hw4/python/needle/data/datasets/cifar10_dataset.py
+++ hw_code/hw4/python/needle/data/datasets/cifar10_dataset.py
@@ -22,8 +22,31 @@ class CIFAR10Dataset(Dataset):
         y - numpy array of labels
         """
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.transforms = transforms
+        if(train):
+            data_list = []
+            labels_list = []
+            for i in range(1,6):
+                data,labels = self.load_data(os.path.join(base_folder, "data_batch_"+str(i)))
+                data_list.append(data)
+                labels_list.append(labels)
+            self.X = np.concatenate(data_list, axis=0)
+            self.y = np.concatenate(labels_list, axis=0)
+        else:
+            self.X, self.y = self.load_data(os.path.join(base_folder, "test_batch"))
+
         ### END YOUR SOLUTION
+
+    def load_data(self, file_name):
+        with open(file_name, 'rb') as fo:
+            cifar_data_file = pickle.load(fo, encoding='bytes')
+            data = cifar_data_file[b'data'] # numpy.ndarray shape: (#samples, 3072)
+            data = data.reshape(-1,3,32,32)
+            labels = cifar_data_file[b'labels'] # a list of ints
+            labels = np.array(labels)
+            return data / 255, labels # divide by 255, so the images are in 0-1 range
+
+
 
     def __getitem__(self, index) -> object:
         """
@@ -31,7 +54,7 @@ class CIFAR10Dataset(Dataset):
         Image should be of shape (3, 32, 32)
         """
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return self.X[index], self.y[index]
         ### END YOUR SOLUTION
 
     def __len__(self) -> int:
@@ -39,5 +62,5 @@ class CIFAR10Dataset(Dataset):
         Returns the total number of examples in the dataset
         """
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return len(self.y)
         ### END YOUR SOLUTION
