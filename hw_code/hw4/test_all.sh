4719075e413538b713b5966767381f0294c4c31e
Wei Zhang <weiz@us.ibm.com>
2024-07-07 14:13:27 -0400

    update test_all.sh

--- hw_code/hw4/test_all.sh
+++ hw_code/hw4/test_all.sh
@@ -2,3 +2,4 @@ source ../dlsys.profile
 export NEEDLE_BACKEND=nd
 export PYTHONPATH=/mnt/nfs/d3nvme0/userhomes/weiz/10714/hw_code/hw4/python
 python3 -m pytest -l -v -k "nd_backend"
+python3 -m pytest -l -v -k "test_cifar10"
