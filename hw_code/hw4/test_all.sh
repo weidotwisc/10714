bf5a48f9b2d2c5c0872a7e2ba34eda2862e6758c
Wei Zhang <weiz@us.ibm.com>
2024-09-30 20:48:35 +0000

    udpate test_all.sh in hw4

--- hw_code/hw4/test_all.sh
+++ hw_code/hw4/test_all.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 source ../dlsys.profile
+# Check if DLSYS_HOME is defined
+if [ -z "$DLSYS_HOME" ]; then
+    echo "Error: DLSYS_HOME environment variable is not defined."
+    exit 1
+fi
+export PYTHONPATH=$PYTHONPATH:$DLSYS_HOME/hw4/python
 export NEEDLE_BACKEND=nd
-export PYTHONPATH=/mnt/nfs/d3nvme0/userhomes/weiz/10714/hw_code/hw4/python
 python3 -m pytest -l -v -k "nd_backend"
 python3 -m pytest -l -v -k "test_cifar10"
 python3 -m pytest -l -v -k "pad_forward"
