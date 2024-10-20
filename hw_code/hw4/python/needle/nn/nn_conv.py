6aba540dbb11c4e349766f98d0ca499c3cfa1835
Wei Zhang <weiz@us.ibm.com>
2024-10-20 08:43:53 -0400

    first implementation of ResNet9, not passing the parameter count test yet

--- hw_code/hw4/python/needle/nn/nn_conv.py
+++ hw_code/hw4/python/needle/nn/nn_conv.py
@@ -5,7 +5,7 @@ from needle.autograd import Tensor
 from needle import ops
 import needle.init as init
 import numpy as np
-from .nn_basic import Parameter, Module
+from .nn_basic import Parameter, Module, BatchNorm2d, ReLU, Sequential, Residual
 
 
 class Conv(Module):
@@ -58,3 +58,44 @@ class Conv(Module):
         return y
 
         ### END YOUR SOLUTION
+
+
+class ConvBN(Module):
+    """
+    Multi-channel 2D convolutional layer
+    IMPORTANT: Accepts inputs in NCHW format, outputs also in NCHW format
+    Only supports padding=same
+    No grouped convolution or dilation
+    Only supports square kernels
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, bias=True, device=None, dtype="float32"):
+        super().__init__()
+        if isinstance(kernel_size, tuple):
+            kernel_size = kernel_size[0]
+        if isinstance(stride, tuple):
+            stride = stride[0]
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        # weiz 2024-10-18 conv->batchnorm2d->relu
+        self.conv = Conv(in_channels, out_channels, kernel_size, stride=stride, bias=bias, device=device, dtype=dtype)
+        self.bn = BatchNorm2d(out_channels, device=device, dtype=dtype)
+        self.relu = ReLU()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.relu(self.bn(self.conv(x)))
+    
+class ResNetBasicBlock(Module):
+    # conv_params_tuple is a tuple of ConvBN parameters, which is a tuple of (in_channels, out_channels, kernel_size and stride)
+    def __init__(self, conv_params_tuple:tuple, bias=True, device=None, dtype="float32"):
+        convbn_list=[]
+        for conv_params in conv_params_tuple:
+            in_channels, out_channels, kernel_size, stride = conv_params
+            convbn = ConvBN(in_channels, out_channels, kernel_size, stride = stride, bias=bias, device=device,dtype=dtype)
+            convbn_list.append(convbn)
+        self.main_path = Sequential(*convbn_list)
+        self.residual_block = Residual(self.main_path)
+
+    def forward(self, x:Tensor) -> Tensor:
+        return self.residual_block(x)
