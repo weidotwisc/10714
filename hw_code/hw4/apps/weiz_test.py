fix PowerScalar gradient() by using Tensor operation instead of NDArray operation.

hw_code/hw4/apps/weiz_test.py
@@ -768,4 +768,36 @@ def test_broadcast_to_forward():
         ),
     )
 
-test_broadcast_to_forward()
+# test_broadcast_to_forward()
+def gradient_check(f, *args, tol=1e-6, backward=False, **kwargs):
+    eps = 1e-4
+    numerical_grads = [np.zeros(a.shape) for a in args]
+    for i in range(len(args)):
+        for j in range(args[i].realize_cached_data().size):
+            args[i].realize_cached_data().flat[j] += eps
+            f1 = float(f(*args, **kwargs).numpy().sum())
+            args[i].realize_cached_data().flat[j] -= 2 * eps
+            f2 = float(f(*args, **kwargs).numpy().sum())
+            args[i].realize_cached_data().flat[j] += eps
+            numerical_grads[i].flat[j] = (f1 - f2) / (2 * eps)
+    if not backward:
+        out = f(*args, **kwargs)
+        computed_grads = [
+            x.numpy()
+            for x in out.op.gradient_as_tuple(ndl.Tensor(np.ones(out.shape)), out)
+        ]
+    else:
+        out = f(*args, **kwargs).sum()
+        out.backward()
+        computed_grads = [a.grad.numpy() for a in args]
+    error = sum(
+        np.linalg.norm(computed_grads[i] - numerical_grads[i]) for i in range(len(args))
+    )
+    assert error < tol
+    return computed_grads
+
+def test_power_scalar_backward():
+    gradient_check(
+        ndl.power_scalar, ndl.Tensor(np.random.randn(5, 4)), scalar=np.random.randint(1)
+    )
+test_power_scalar_backward()