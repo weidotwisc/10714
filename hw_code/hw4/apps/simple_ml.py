ecbd06d0f8555478ff92979e235ae1dc45d02250
Wei Zhang <weiz@us.ibm.com>
2024-12-01 22:20:49 -0500

    (1) make sure the hidden states are detached in RNN-based LM training and (2) add parity check in utils.py

--- hw_code/hw4/apps/simple_ml.py
+++ hw_code/hw4/apps/simple_ml.py
@@ -294,9 +294,20 @@ def epoch_general_ptb(data, model, seq_len=40, loss_fn=nn.SoftmaxLoss(), opt=Non
             opt.reset_grad()
             X, y = batch
             #X,y = ndl.Tensor(X, device=device), ndl.Tensor(y, device=device)
-            out, h = model(X,h)
+            if(X.shape[0] == seq_len):
+                out, h = model(X,h)
+            else:
+                out, h = model(X, None)
+            # detach h
+            if isinstance(h, tuple):
+                h = tuple([h_.detach() for h_ in h])
+            else:
+                h = h.detach()
+            
             correct += np.sum(np.argmax(out.numpy(), axis=1) == y.numpy())
             loss = loss_fn(out, y)
+            
+            
             print("training loss ", loss)
             total_loss += loss.data.numpy() * y.shape[0]
             total_sample_num += y.shape[0]
@@ -312,8 +323,18 @@ def epoch_general_ptb(data, model, seq_len=40, loss_fn=nn.SoftmaxLoss(), opt=Non
             X, y = batch
             #X,y = ndl.Tensor(X, device=device), ndl.Tensor(y, device=device)
             out, h = model(X, h)
+            # detach h
+            if isinstance(h, tuple):
+                h = tuple([h_.detach() for h_ in h])
+            else:
+                h = h.detach()
+            
             correct += np.sum(np.argmax(out.numpy(), axis=1) == y.numpy())
+            
+            
             loss = loss_fn(out, y)
+            
+            
             print("eval loss ", loss)
             total_loss += loss.data.numpy() * y.shape[0]
             total_sample_num += y.shape[0]
