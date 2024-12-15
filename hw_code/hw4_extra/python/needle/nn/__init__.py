646f26ba6dd4ceac9c2473160aa205aa03ab716a
Wei Zhang <weiz@us.ibm.com>
2024-12-14 22:04:48 -0500

    import nn_transformer

--- hw_code/hw4_extra/python/needle/nn/__init__.py
+++ hw_code/hw4_extra/python/needle/nn/__init__.py
@@ -1,3 +1,4 @@
 from .nn_basic import *
 from .nn_conv import *
 from .nn_sequence import *
+from .nn_transformer import * # weiz 2024-12-14 add nn_transformer, wasn't in hw4 but now available in hw4_extra
