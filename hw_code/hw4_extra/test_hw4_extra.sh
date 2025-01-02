087ada6ac7a1f33d635d623430f1c8f756881827
Wei Zhang <weiz@us.ibm.com>
2025-01-02 18:06:06 -0500

    update readme

--- hw_code/hw4_extra/test_hw4_extra.sh
+++ hw_code/hw4_extra/test_hw4_extra.sh
@@ -2,3 +2,5 @@ source ../dlsys.profile
 export PYTHONPATH=$DLSYS_HOME/hw4_extra/python
 # part 1 multi-head attention activation layer
 python3 -m pytest ./tests/hw4_extra/ -l -v -k   "attention_activation" # weiz 2024-12-29, mom's birthday :)
+# part 2 implementing the self-attention layer with trainable parameters
+python3 -m pytest ./tests/hw4_extra  -l -v -k "attention_layer" # weiz 2025-01-02
