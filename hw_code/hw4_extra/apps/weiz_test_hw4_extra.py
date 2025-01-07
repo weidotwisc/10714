b34e5b27f6899eb1db72a376cf04e56b92f69556
Wei Zhang <weiz@us.ibm.com>
2025-01-07 15:13:36 +0000

    train 10 epochs

--- hw_code/hw4_extra/apps/weiz_test_hw4_extra.py
+++ hw_code/hw4_extra/apps/weiz_test_hw4_extra.py
@@ -196,7 +196,7 @@ def test_transformer_language_model():
     corpus = ndl.data.Corpus("data/ptb")
     train_data = ndl.data.batchify(corpus.train, batch_size=256, device=device, dtype="float32")
     model = LanguageModel(20, len(corpus.dictionary), hidden_size=32, num_layers=1, seq_model='transformer', seq_len=20, device=device)
-    train_ptb(model, train_data, seq_len=20, n_epochs=1, device=device, lr=0.003, optimizer=ndl.optim.Adam)
+    train_ptb(model, train_data, seq_len=20, n_epochs=10, device=device, lr=0.003, optimizer=ndl.optim.Adam)
     evaluate_ptb(model, train_data, seq_len=20, device=device)
 
 test_transformer_language_model()
